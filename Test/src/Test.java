9442bf3596c14e587901fc0455e9435fdb340d52
즐겨찾기 <gmspell@naver.com>
2017-04-04 09:32:07 +0900

    gitignore

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.print("World");
-<<<<<<< HEAD
-
-=======
+		Scanner scanf = new Scanner(System.in);
+		String Text;
+		
+		System.out.print("Hello ");
+		System.out.println("World");
 		System.out.println("바보");
->>>>>>> refs/heads/hotfix
+		System.out.println(" 암호화 하실 코드를 입력해주세요.! " );
+		Text = scanf.nextLine();
+		System.out.println(Text+"입니다.");
+		
+		
 	}
 
 }
