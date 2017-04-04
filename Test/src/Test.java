b885d8a28d51c536ff7d700dd9a8a60b79f68afe
즐겨찾기 <gmspell@naver.com>
2017-04-04 09:38:43 +0900

    New Line

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -6,13 +6,15 @@ public class Test {
 		Scanner scanf = new Scanner(System.in);
 		String Text;
 		
+		
+		
 		System.out.print("Hello ");
 		System.out.println("World");
 		System.out.println("바보");
 		System.out.println(" 암호화 하실 코드를 입력해주세요.! " );
 		Text = scanf.nextLine();
 		System.out.println(Text+"입니다.");
-		
+		System.out.println("자바했다가 c했다가 "+Text);
 		
 	}
 
